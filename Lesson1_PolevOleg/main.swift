ec78182237bcb34cdcaccb5fcf843720756c3b55
OlegPoleff <67762168+OlegPoleff@users.noreply.github.com>
2021-01-21 15:13:17 +0300

    Update main.swift

--- Lesson1_PolevOleg/main.swift
+++ Lesson1_PolevOleg/main.swift
@@ -12,25 +12,25 @@ import Cocoa
 // Решить квадратное уранение.
 // ax^2+bx+c=0
 
-//var a: Double = 2
-//var b: Double = 5
-//var c: Double = 2
-//let discriminant = (b*b)-(4*a*c) // дискриминант
-//var x1: Double = 0
-//var x2: Double = 0
-//
-//if discriminant > 0 {
-//    x1 = (-b+sqrt(discriminant))/(2 * a)
-//    x2 = (-b-sqrt(discriminant))/(2 * a)
-//    print("Уравнение имеет два корня. x1: \(x1)\n = \(x2)")
-//}
-//else if discriminant == 0 {
-//    let x: Double = -b/(2 * a)
-//    print("x: \(x)\n")
-//}
-//else if discriminant < 0 {
-//    print("Корней нет")
-//}
+var a: Double = 2
+var b: Double = 5
+var c: Double = 2
+let discriminant = (b*b)-(4*a*c) // дискриминант
+var x1: Double = 0
+var x2: Double = 0
+
+if discriminant > 0 {
+   x1 = (-b+sqrt(discriminant))/(2 * a)
+   x2 = (-b-sqrt(discriminant))/(2 * a)
+   print("Уравнение имеет два корня. x1: \(x1)\n = \(x2)")
+}
+else if discriminant == 0 {
+   let x: Double = -b/(2 * a)
+   print("x: \(x)\n")
+}
+else if discriminant < 0 {
+   print("Корней нет")
+}
 
 //Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
 
